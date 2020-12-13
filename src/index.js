569d945919c3eadf3f7666293d757a5ecd672a64
Vinicius NUnes <viniciusnunes02612@gmail.com>
2020-12-13 02:29:00 -0300

    estilando com Bootstrap

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+function validator(){
+    var nome = document.getElementById("#user");
+    var senha = document.getElementById("password")
+    
+    if(nome.value == ''){
+        console.log("user invalid")
+    }
+}
+
